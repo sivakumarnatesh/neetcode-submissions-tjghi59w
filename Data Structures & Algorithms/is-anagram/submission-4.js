72dcc700c5aba4aa832630973f5b2f74196feb79
Sivakumar <54715135+sivakumarnatesh@users.noreply.github.com>
2026-09-05 16:17:54 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        // return s.split('').sort().join('').toLowerCase() === t.split('').sort().join('').toLowerCase() ? true : false;
+        let obj1 = {};
+        let obj2 = {};
+
+        if(s.length !== t.length){
+            return false;
+        } else {
+            for(let i = 0; i < s.length; i++){
+                obj1[s[i]] = (obj1[s[i]] || 0) + 1;
+                obj2[t[i]] = (obj2[t[i]] || 0) + 1;
+            }
+
+            for(const val in obj1){
+                if(obj1[val] !== obj2[val]){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
